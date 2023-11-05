c167d4b331941dbba59b9c9278bc1e8a403842ad
Himalaya <110767053+HimalayaJhala@users.noreply.github.com>
2023-11-05 16:04:37 +0530

    Draft4

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,73 @@
 // CustomComponent.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '/styles/index.module.css';
 import Image from 'next/image';
 import Head from 'next/head';
+import ReactPlayer from 'react-player';
 
+const YourComponent = () => {
+  const [isClient, setIsClient] = useState(false);
 
-export default function Home ()  {
-    return (
-        <div className={styles.bodyContainer} >
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
+  
+
+  return (
+    <div className={styles.bodyContainer}>
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Tilt+Warp&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda&display=swap" rel="stylesheet" />
       </Head>
 
-            <div className={styles.b1}>
-                <div className={styles.heading}>
-                    <h1 className={styles.we_belive}>WE BELIEVE DEVELOPER SHAPE THE <span className={styles.future}>FUTURE </span></h1>
-                </div>
-            </div>
-           
+      <div className={styles.scrollBehavior}>
+        <div className={styles.b1}>
+          <div className={styles.heading}>
+            <h1 className={styles.we_belive}>
+                WE BELIEVE DEVELOPER SHAPE THE <span className={styles.future}>FUTURE </span>
+            </h1>
+          </div>
+        </div>
+
        
-            <Image className={styles.pic} src="/download.jpeg" alt="profilepicture" width={500} height={500} />
+<Image className={styles.pic} src="/download.jpeg" alt="profilepicture" width={500} height={500} />
+       
 
-
-                <p className={styles.heading1}>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-            </div>
-    );
+        <div className={styles.Firstbox}>
+          <div className={styles.playerwrapper}>
+          <Image  className={styles.iphone} src="/iPhone15.png" alt="phone" width={435} height={790} />
+            {isClient && (
+              <div  style={{ width :'445px',height :'780px' , position: 'relative',   zIndex: 0,  borderRadius: '99px', overflow: 'hidden' , left : '-10px',  top : '37px' }}>    
+                <ReactPlayer
+                  className="react-player"
+                  url="/Screen1.mp4"
+                  width="455px"
+                  height="720px"
+                  playing={true}
+                  loop={true}
+                  controls={false}
+                  muted={true}
+                />
+              </div>
+            )}
+          </div>
+          
+            
+          
+          <p className={styles.heading1}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard
+            dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default YourComponent;
