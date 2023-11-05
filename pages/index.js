34afb001cc17056d0cdde2f1b09b1bdc69650572
Himalaya <110767053+HimalayaJhala@users.noreply.github.com>
2023-11-05 11:03:31 +0530

    Draft2e

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,33 @@
 // CustomComponent.js
 import React from 'react';
 import styles from '/styles/index.module.css';
+import Image from 'next/image';
+import Head from 'next/head';
 
 
 export default function Home ()  {
     return (
         <div className={styles.bodyContainer} >
+
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Tilt+Warp&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda&display=swap" rel="stylesheet" />
+      </Head>
+
             <div className={styles.b1}>
                 <div className={styles.heading}>
                     <h1 className={styles.we_belive}>WE BELIEVE DEVELOPER SHAPE THE <span className={styles.future}>FUTURE </span></h1>
                 </div>
             </div>
+           
+       
+            <Image className={styles.pic} src="/download.jpeg" alt="profilepicture" width={500} height={500} />
 
-            <div>
-                <img className={styles.pic} src="download.jpeg" alt="profilepicture" />
+                {/* <Image className={styles.pic} src="download.jpeg" alt="profilepicture  width{500}   height{500}" /> */}
                 <p className={styles.heading1}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
             </div>
-        </div>
     );
 };
