bc542889c5ce4180f29f0fbc087095c2978bf9f9
Himalaya <110767053+HimalayaJhala@users.noreply.github.com>
2023-11-07 12:20:02 +0530

    Draft9

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from '/styles/index.module.css';
 import style from '/styles/social.module.css';
+import styleg from '/styles/globals.css';
 import Image from 'next/image';
 import Head from 'next/head';
 import ReactPlayer from 'react-player';
@@ -15,68 +16,67 @@ const YourComponent = () => {
 
   return (
     <div className={styles.bodyContainer}>
-         <div className={styles.scrollBehavior}>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Tilt+Warp&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda&display=swap" rel="stylesheet" />
-      </Head>
+      <div className={styles.scrollBehavior}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Tilt+Warp&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda&display=swap" rel="stylesheet" />
+        </Head>
 
-     
+
         <div className={styles.b1}>
           <div className={styles.heading}>
             <h1 className={styles.we_belive}>
-                WE BELIEVE DEVELOPER SHAPE THE <span className={styles.future}>FUTURE </span>
+              WE BELIEVE DEVELOPER SHAPE THE <span className={styles.future}>FUTURE </span>
             </h1>
           </div>
         </div>
 
-<div className={style.centerpic}>
-<Image className={styles.pic} src="/download.jpeg" alt="profilepicture" width={500} height={500} />
-</div>
-
-      
-<div className={styles.buttond}>
-<a href="/path-to-your-resume.pdf" download="Your_Resume_Name.pdf" className={styles.downloadResumeButton}>
-  Download Resume
-</a>
-</div>
+        <div className={style.centerpic}>
+          <Image className={styles.pic} src="/download.jpeg" alt="profilepicture" width={500} height={500} />
+        </div>
 
 
-
-<div className={styles.socialc}>
-<div className={styles.social}>
-
-
-<div className={styles.socials1}>
-    <a href="https://www.linkedin.com/in/himalaya-jhala-879542231/">
-     <Image src="/linkedin.png" alt="leetcode" width={50} height={50} />
-    </a>
-</div>
-
-<div className={styles.socials2}>
-<a href="https://leetcode.com/HimalayaJhala/">
-  <Image src="/leetcode.png" alt="leetcode" width={50} height={50} />
-</a>
-</div>
-
-<div className={styles.socials3}>
-<a href="https://github.com/HimalayaJhala">
-  <Image src="/github.png" alt="leetcode" width={50} height={50} />
-</a>
-</div>
-
-</div>
-
-</div>
+        <div className={styles.buttond}>
+          <a href="/path-to-your-resume.pdf" download="Your_Resume_Name.pdf" className={styles.downloadResumeButton}>
+            Download Resume
+          </a>
+        </div>
 
 
-</div>
+
+        <div className={styles.socialc}>
+          <div className={styles.social}>
+
+
+            <div className={styles.socials1}>
+              <a href="https://www.linkedin.com/in/himalaya-jhala-879542231/">
+                <Image src="/linkedin.png" alt="leetcode" width={50} height={50} />
+              </a>
+            </div>
+
+            <div className={styles.socials2}>
+              <a href="https://leetcode.com/HimalayaJhala/">
+                <Image src="/leetcode.png" alt="leetcode" width={50} height={50} />
+              </a>
+            </div>
+
+            <div className={styles.socials3}>
+              <a href="https://github.com/HimalayaJhala">
+                <Image src="/github.png" alt="leetcode" width={50} height={50} />
+              </a>
+            </div>
+
+          </div>
+
+        </div>
+
+
         <div className={styles.Firstbox}>
           <div className={styles.playerwrapper}>
-          <Image  className={styles.iphone} src="/iPhone15.png" alt="phone" width={435} height={790} />
+            <Image className={styles.iphone} src="/iPhone15.png" alt="phone" width={435} height={790} />
             {isClient && (
-               <div  style={{ width :'445px',height :'780px' , position: 'relative',   zIndex: 0,   overflow: 'hidden' ,   top : '37px' ,   borderradius: '150px' }}>    
+              <div style={{ width: '445px', height: '780px', position: 'relative', zIndex: 0, overflow: 'hidden', top: '37px', borderradius: '150px' }}>
                 <ReactPlayer
                   className="react-player"
                   url="/Screen1.mp4"
@@ -93,9 +93,9 @@ const YourComponent = () => {
 
 
           <div className={styles.playerwrapper1}>
-          <Image  className={styles.iphone} src="/iPhone15.png" alt="phone" width={365} height={670} />
+            <Image className={styles.iphone} src="/iPhone15.png" alt="phone" width={365} height={670} />
             {isClient && (
-                <div  style={{  width :'370px',height :'620px', position: 'relative',    overflow: 'hidden' ,   top : '32px' , borderradius: '1500px' }}>    
+              <div style={{ width: '370px', height: '620px', position: 'relative', overflow: 'hidden', top: '32px', borderradius: '1500px' }}>
                 <ReactPlayer
                   className="react-player"
                   url="/Screen1.mp4"
@@ -105,71 +105,68 @@ const YourComponent = () => {
                   loop={true}
                   controls={false}
                   muted={true}
-                  style={{borderRadius: '100px'}}
-                
+                  style={{ borderRadius: '100px' }}
+
                 />
-               </div>
+              </div>
             )}
           </div>
 
 
           <div className={styles.para}>
-          <p className={styles.heading1}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard
-            dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
-            specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
-            versions of Lorem Ipsum.
-          </p>
+            <p className={styles.heading1}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard
+              dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+              specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+              Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+            </p>
           </div>
         </div>
-     
-
-      <div className={styles.laptopdiv}>
-         <Image className={styles.laptop} src="/macbookp.png" alt="laptop" width={1284} height={792} />
-         {isClient && (
-                <div  style={{  position: 'absolute'      }}>    
-                <ReactPlayer
-                  className={styles.reactplayer1}
-                  url="/Recording.mp4"
-                  width="960px"
-                  height="762px"
-                  playing={true}
-                  loop={true}
-                  controls={false}
-                  muted={true}
-                  // style={{borderRadius: '100px'}}
-                />
-               </div>
-              )}
-               </div>
 
 
-               <div className={styles.laptopdiv1}>
-         <Image className={styles.laptop1} src="/macbookp.png" alt="laptop" width={394} height={252} />
-         {isClient && (
-                <div  style={{
-                 }}>    
-                <ReactPlayer
-          
-                  className={styles.reactplayer2}
-                  url="/Recording.mp4"
-                  width="399px"
-                  height="194px"
-                  playing={true}
-                  loop={true}
-                  controls={false}
-                  muted={true}
-                  
-                 
-                />
-               </div>
-              )}
-               </div>
+        <div className={styles.laptopdiv}>
+          <Image className={styles.laptop} src="/macbookp.png" alt="laptop" width={1284} height={792} />
+          {isClient && (
+            <div style={{ position: 'absolute' }}>
+              <ReactPlayer
+                className={styles.reactplayer1}
+                url="/Recording.mp4"
+                width="960px"
+                height="762px"
+                playing={true}
+                loop={true}
+                controls={false}
+                muted={true}
+              // style={{borderRadius: '100px'}}
+              />
+            </div>
+          )}
+        </div>
 
 
-               <div className={styles.para1}>
+        <div className={styles.laptopdiv1}>
+          <Image className={styles.laptop1} src="/macbookp.png" alt="laptop" width={394} height={252} />
+          {isClient && (
+            <div style={{
+            }}>
+              <ReactPlayer
+
+                className={styles.reactplayer2}
+                url="/Recording.mp4"
+                width="399px"
+                height="194px"
+                playing={true}
+                loop={true}
+                controls={false}
+                muted={true}
+              />
+            </div>
+          )}
+        </div>
+
+        <div className={styles.para1}>
           <p className={styles.heading2}>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard
             dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
@@ -178,14 +175,12 @@ const YourComponent = () => {
             Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
             versions of Lorem Ipsum.
           </p>
-          </div>
+        </div>
 
-
-
+      </div>
     </div>
-             
 
-    
+
   );
 };
 
