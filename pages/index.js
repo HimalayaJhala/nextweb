c4a1f36b349e93e4e1b370dc0148ea9f62359b91
Himalaya <110767053+HimalayaJhala@users.noreply.github.com>
2024-02-28 21:53:10 +0530

    Animation

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ const YourComponent = () => {
       const mobileTl = gsap.timeline({ yoyo: true  });
      
       mobileTl.to(imageRef.current, { yoyo: true, y: 200, duration: 3  ,delay: 1.0, top : 100 });
-      const para = gsap.to(paraRef.current, { yoyo: true, y : -100 ,duration: 3, delay: 1.5, opacity: 0.9 } , 1.5);
+      const para = gsap.to(paraRef.current, { yoyo: true, y : -100 ,duration: 3, delay: 0.5, opacity: 0.9 });
       //Pause the main timeline (tl) before playing mobileTl
       tl.pause();
       mobileTl.play();
