9582d1fba87c6516ee0a938259915285b5b070b4
Himalaya <110767053+HimalayaJhala@users.noreply.github.com>
2024-02-28 22:15:06 +0530

    Resolving timing issue

--- pages/index.js
+++ pages/index.js
@@ -34,8 +34,8 @@ const YourComponent = () => {
       //Create mobile-specific timeline
       const mobileTl = gsap.timeline({ yoyo: true  });
      
-      mobileTl.to(imageRef.current, { yoyo: true, y: 200, duration: 7  ,delay: 2, top : 100 });
-      const para = gsap.to(paraRef.current, { yoyo: true, y : -100 ,duration: 7, delay: 2, opacity: 1 });
+      mobileTl.to(imageRef.current, { yoyo: true, y: 200, duration: 8  ,delay: 2, top : 100 });
+      const para = gsap.to(paraRef.current, { yoyo: true, y : -100 ,duration: 7, delay: 2, opacity: 1 } , 1);
       //Pause the main timeline (tl) before playing mobileTl
       tl.pause();
       mobileTl.play();
